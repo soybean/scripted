312dd80a7a84ad18da0c3e69035bd848bdcbe6ed
Melanie Sawyer <melaniensawyer@gmail.com>
2018-06-29 20:25:56 -0400

    add bootstrap to pages, add form page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return render_template('index.html')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route("/submit")
+def submit():
+    return render_template('form.html')
     
 if __name__ == "__main__":
     app.run()
