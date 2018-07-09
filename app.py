fed8605b77f9872bea403f5f42cf2879d18055d2
Cynthia Zhou <cyntzhou@gmail.com>
2018-07-08 20:52:17 -0400

    Connect Flask with MySQL localhost (but not janice's server yet)

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
+# from flask_sqlalchemy import SQLAlchemy
+from flaskext.mysql import MySQL
+from config import *
 
 app = Flask(__name__)
+mysql = MySQL()
 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = MYSQL_DATABASE_USER
+app.config['MYSQL_DATABASE_PASSWORD'] = MYSQL_DATABASE_PASSWORD
+app.config['MYSQL_DATABASE_DB'] = MYSQL_DATABASE_DB
+app.config['MYSQL_DATABASE_HOST'] = MYSQL_DATABASE_HOST
+mysql.init_app(app)
+
+# Views
 @app.route("/")
 def gallery():
     return render_template('gallery.html')
 
-@app.route("/submit")
+@app.route("/submit", methods=['GET', 'POST'])
 def submit():
-    return render_template('form.html')
-    
+    if request.method == 'POST':
+        try:
+            name = request.form['projectName']
+            if name:
+                add_project(name)
+                return json.dumps({'html':'<span>All fields good !!</span>'})
+            else:
+                return json.dumps({'html':'<span>Enter the required fields</span>'})
+        except Exception as e:
+            return json.dumps({'error':str(e)})
+    else:
+        return render_template('form.html')
+
+# Database functions
+def add_project(name):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(
+        """INSERT INTO
+            projects (name)
+        VALUES (%s)""", (name))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ config_template.py
@@ -0,0 +1,6 @@
+# Copy this file, replace the strings, and rename the file config.py
+MYSQL_DATABASE_USER = ''
+MYSQL_DATABASE_PASSWORD = ''
+MYSQL_DATABASE_DB = ''
+MYSQL_DATABASE_HOST = ''
+MYSQL_DATABASE_HOST = ''
