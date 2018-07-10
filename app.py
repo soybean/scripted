4f3f6a4fe3a3a722753f54c0c7c1173e4fd2850f
Cynthia Zhou <cyntzhou@gmail.com>
2018-07-09 23:06:58 -0400

    Connected to AWS RDS DB instance using SQLALCHEMY, Created Project and Tags classes

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, json, request
-# from flask_sqlalchemy import SQLAlchemy
-from flaskext.mysql import MySQL
-from config import *
+from flask_sqlalchemy import SQLAlchemy
+# from config import *
+from app import db, app
+from app.models import Project
+
+app.debug = True
+app.secret_key = 'bunnies'
 
 app = Flask(__name__)
-mysql = MySQL()
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = MYSQL_DATABASE_USER
-app.config['MYSQL_DATABASE_PASSWORD'] = MYSQL_DATABASE_PASSWORD
-app.config['MYSQL_DATABASE_DB'] = MYSQL_DATABASE_DB
-app.config['MYSQL_DATABASE_HOST'] = MYSQL_DATABASE_HOST
-mysql.init_app(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 # Views
 @app.route("/")
@@ -21,29 +19,22 @@ def gallery():
 @app.route("/submit", methods=['GET', 'POST'])
 def submit():
     if request.method == 'POST':
-        try:
-            name = request.form['projectName']
-            if name:
-                add_project(name)
-                return json.dumps({'html':'<span>All fields good !!</span>'})
-            else:
-                return json.dumps({'html':'<span>Enter the required fields</span>'})
-        except Exception as e:
-            return json.dumps({'error':str(e)})
+        name = request.form['projectName']
+        if name:
+            try:
+                db.session.add(Project(name=name))
+                db.session.commit()
+                db.session.close()
+                return json.dumps({'html':'<span>Project Added</span>'})
+            except Exception as e:
+                db.session.rollback()
+                return json.dumps({'error':str(e)})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
     else:
         return render_template('form.html')
 
 # Database functions
-def add_project(name):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute(
-        """INSERT INTO
-            projects (name)
-        VALUES (%s)""", (name))
-    conn.commit()
-    cursor.close()
-    conn.close()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- config_template.py
+++
@@ -1,6 +0,0 @@
-# Copy this file, replace the strings, and rename the file config.py
-MYSQL_DATABASE_USER = ''
-MYSQL_DATABASE_PASSWORD = ''
-MYSQL_DATABASE_DB = ''
-MYSQL_DATABASE_HOST = ''
-MYSQL_DATABASE_HOST = ''
